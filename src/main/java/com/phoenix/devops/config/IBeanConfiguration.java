04dd4c1999ceb858c03a016504c5f0a22e1c50be
wjj-phoenix <3951817834@qq.com>
2025-02-17 22:09:33 +0800

    feat:实现项目常用配置项

---
+++ src/main/java/com/phoenix/devops/config/IBeanConfiguration.java
@@ -0,0 +1,25 @@
+package com.phoenix.devops.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * @author wjj-phoenix
+ * @since 2025-02-17
+ */
+@Configuration
+public class IBeanConfiguration {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SessionRegistry sessionRegistry() {
+        return new SessionRegistryImpl();
+    }
+}
